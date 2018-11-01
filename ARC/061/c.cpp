6533df2a526bad88fb0c098f9f0be43593fca624
komoriyuichiro <thekomori1113@gmail.com>
2018-11-01 22:46:13 +0900

    [wip] ARC061C

---
+++ ARC/061/c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  int sum = 0;
+
+  int k = 0;
+  for (int i = 0; i < S.size(); ++i) {
+  }
+  return 0;
+}
