4eac7fd0ed15705edc7152dd5e12d855a41c2b8a
komoriyuichiro <thekomori1113@gmail.com>
2018-11-14 20:52:49 +0900

    [modify] Add include at templete.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
 using namespace std;
+
+typedef long long ll;
+
 
 int main() {
   int N;
