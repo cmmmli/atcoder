221e9b14e2574ecb686751b1d8cdddf39d94d963
komoriyuichiro <thekomori1113@gmail.com>
2018-11-15 22:12:21 +0900

    [AC] ABC101B

---
+++ ABC/101/b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <ctype.h>
+using namespace std;
+
+typedef long long ll;
+
+int dx[4] = { 0, -1, 1, 0 };
+int dy[4] = { -1, 0, 0, 1 };
+
+int digitSum(int n) {
+  int sum = 0;
+  while (n > 0) {
+    sum += n % 10;
+    n /= 10;
+  }
+  return sum;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int sn;
+  sn = digitSum(n);
+  if (n % sn == 0) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
+}
