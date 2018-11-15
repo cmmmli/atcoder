e6f4667f7d27da679202ab33d56e6d8901c17b09
komoriyuichiro <thekomori1113@gmail.com>
2018-11-15 09:01:00 +0900

    [AC] ABC107A

---
+++ ABC/107/a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+using namespace std;
+
+typedef long long ll;
+
+int dx[4] = { 0, -1, 1, 0 };
+int dy[4] = { -1, 0, 0, 1 };
+
+int main() {
+  int N;
+  int i;
+  cin >> N >> i;
+  cout << N - i + 1 << endl;
+
+  return 0;
+}
