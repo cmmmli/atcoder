37f5dd1507fc1644face5ab7855db455cdd5d528
komoriyuichiro <thekomori1113@gmail.com>
2018-11-16 22:16:21 +0900

    [AC] ABC089A

---
+++ ABC/089/a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <ctype.h>
+using namespace std;
+
+typedef long long ll;
+
+int dx[4] = { 0, -1, 1, 0 };
+int dy[4] = { -1, 0, 0, 1 };
+
+int main() {
+  int n;
+  cin >> n;
+  cout << n / 3 << endl;
+  return 0;
+}
