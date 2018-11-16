04bfedfd7305f8d90ba94c411d415f284ee99a12
komoriyuichiro <thekomori1113@gmail.com>
2018-11-16 22:18:57 +0900

    [AC] ABC087A

---
+++ ABC/087/a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <ctype.h>
+using namespace std;
+
+typedef long long ll;
+
+int dx[4] = { 0, -1, 1, 0 };
+int dy[4] = { -1, 0, 0, 1 };
+
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+  x -= a;
+  int res = x % b;
+  cout << res << endl;
+  return 0;
+}
