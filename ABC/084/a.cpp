591f46d91c881e8235be477ea10bfb97e752d98f
komoriyuichiro <thekomori1113@gmail.com>
2018-11-17 18:21:00 +0900

    [AC] ABC084A

---
+++ ABC/084/a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <ctype.h>
+using namespace std;
+
+typedef long long ll;
+
+int dx[4] = { 0, -1, 1, 0 };
+int dy[4] = { -1, 0, 0, 1 };
+
+int m[1010];
+
+int main() {
+  int n;
+  cin >> n;
+  cout << 48 - n << endl;
+  return 0;
+}
