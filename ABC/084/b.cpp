d16b26df72376451436be666c8e150d7b397cd88
komoriyuichiro <thekomori1113@gmail.com>
2018-11-19 00:52:37 +0900

    [AC] ABC084B

---
+++ ABC/084/b.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int m[1010];
+
+int main() {
+  int a, b;
+  string s;
+  cin >> a >> b;
+  cin >> s;
+
+  int n = 0;
+  vector<int> x = { 0, 0 };
+  for (int i = 0; i != s.size(); ++i) {
+    if (s[i] == '-') {
+      ++n;
+      continue;
+    }
+    if (n >= 2) break;
+    ++x[n];
+  }
+
+  if (n == 1 && x[0] == a && x[1] == b) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
+}
