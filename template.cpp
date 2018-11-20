43c9393d1361f287c454e53c39cbd5463d3b92f4
komoriyuichiro <thekomori1113@gmail.com>
2018-11-20 09:27:06 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -5,12 +5,20 @@
 #include <iomanip>
 #include <cmath>
 #include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
 using namespace std;
 
 typedef long long ll;
+typedef pair<int, int> P;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
 
-int dx[4] = { 0, -1, 1, 0 };
-int dy[4] = { -1, 0, 0, 1 };
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
 
 int main() {
   int N;
