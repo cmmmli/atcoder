51a6b36c750576474c0890eeb2db2428ed17d518
komoriyuichiro <thekomori1113@gmail.com>
2018-12-09 16:15:59 +0900

    [AC] ABC115A

---
+++ ABC/115/a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int n;
+  cin >> n;
+
+  if (n == 25) {
+    cout << "Christmas" << endl;
+  } else if (n == 24) {
+    cout << "Christmas Eve" << endl;
+  } else if (n == 23) {
+    cout << "Christmas Eve Eve" << endl;
+  } else if (n == 22) {
+    cout << "Christmas Eve Eve Eve" << endl;
+  }
+  return 0;
+}
