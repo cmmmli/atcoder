01b785f8537ce13291cb4268ddcd8d7748613662
komoriyuichiro <thekomori1113@gmail.com>
2018-12-26 21:27:39 +0900

    [AC] ABC108C

---
+++ ABC/108/c.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  ll ans = pow(n / k, 3);
+  if (k % 2 == 0) {
+    ans += pow((n / (k / 2)) - (n / k), 3);
+  }
+
+  cout << ans << endl;
+  return 0;
+}
