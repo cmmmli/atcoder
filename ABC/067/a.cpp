07e9993a758fed04f97b514124c26e6b46f2802d
komoriyuichiro <thekomori1113@gmail.com>
2019-01-03 04:41:47 +0900

    [AC] ABC067A

---
+++ ABC/067/a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  if (a % 3 == 0 || b % 3 == 0 || (a + b) % 3 == 0) cout << "Possible" << endl;
+  else cout << "Impossible" << endl;
+  return 0;
+}
