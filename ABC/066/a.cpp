56c82ad595e1b9363348f18c2e2246ad6b0e7629
komoriyuichiro <thekomori1113@gmail.com>
2019-01-03 04:53:56 +0900

    [AC] ABC066A

---
+++ ABC/066/a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  vector<int> a(3);
+  for (int i = 0; i < 3; ++i) cin >> a[i];
+  sort(a.begin(), a.end());
+  cout << a[0] + a[1] << endl;
+  return 0;
+}
