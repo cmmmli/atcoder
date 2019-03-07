6f794c937d2f473e7697d9218b010955e9076659
komoriyuichiro <thekomori1113@gmail.com>
2019-03-07 13:51:32 +0900

    [AC] ABC64B

---
+++ ABC/064/b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int n; cin >> n;
+  int maxval = -1, minval = INF;
+  int tmp;
+  for (int i = 0; i < n; ++i) {
+    cin >> tmp;
+    minval = min(minval, tmp);
+    maxval = max(maxval, tmp);
+  }
+  cout << maxval - minval << endl;
+  return 0;
+}
