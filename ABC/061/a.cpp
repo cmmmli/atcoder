a69dcf1b70c8fc3049aed050ceb312dc79276a44
komoriyuichiro <thekomori1113@gmail.com>
2019-03-13 21:43:54 +0900

    [AC] ABC061A

---
+++ ABC/061/a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int a, b, c; cin >> a >> b >> c;
+  string ans = "No";
+  if (c >= a && c <= b) ans = "Yes";
+  cout << ans << endl;
+  return 0;
+}
