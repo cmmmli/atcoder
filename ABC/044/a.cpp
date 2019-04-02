8d748fc3ebb1ee49f8402e736b55ebfbf18d2cbc
komoriyuichiro <thekomori1113@gmail.com>
2019-04-02 21:09:59 +0900

    [AC] ABC/044/a.cpp

---
+++ ABC/044/a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int n, k, x, y; cin >> n >> k >> x >> y;
+  int res = min(k, n) * x + max(n - k, 0) * y;
+  cout << res << endl;
+  return 0;
+}
