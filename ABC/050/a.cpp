403e2bf4c8c6f797f56ae741499bb3973b1136ed
komoriyuichiro <thekomori1113@gmail.com>
2019-04-02 20:59:35 +0900

    [AC] ABC/050/a.cpp

---
+++ ABC/050/a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int a, b; char op; cin >> a >> op >> b;
+  if (op == '+') cout << a + b << endl;
+  else cout << a - b << endl;
+  return 0;
+}
