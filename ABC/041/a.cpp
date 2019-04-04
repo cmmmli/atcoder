624fc8ec011b326a3f2986f6a3aaf8aa681004d5
komoriyuichiro <thekomori1113@gmail.com>
2019-04-04 16:59:37 +0900

    [AC] ABC/041/a.cpp

---
+++ ABC/041/a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  string s; int i; cin >> s >> i;
+  cout << s[i - 1] << endl;
+  return 0;
+}
