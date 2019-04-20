fd07889e35cd2ac5b2337d769ed206e1c5d8ab2f
komoriyuichiro <thekomori1113@gmail.com>
2019-04-20 23:59:38 +0900

    [AC] ABC/024/a.cpp

---
+++ ABC/024/a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+#include <math.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int a, b, c, k, s, t;
+  cin >> a >> b >> c >> k >> s >> t;
+  int sum = s + t;
+  if (sum >= k) cout << s * a + t * b - sum * c << endl;
+  else cout << s * a + t * b << endl;
+  return 0;
+}
