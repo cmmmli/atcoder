1b044be0baa3f4f890fdd874512e34f36f742b7a
komoriyuichiro <thekomori1113@gmail.com>
2019-05-19 21:11:09 +0900

    [AC] ABC/126/a.cpp

---
+++ ABC/126/a.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+#include <math.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int n, k; cin >> n >> k;
+
+  for (int i = 0; i < n; ++i) {
+    char c; cin >> c;
+    if (i == k - 1) cout << char(c + 32);
+    else cout << c;
+  }
+  cout << endl;
+
+
+  return 0;
+}
