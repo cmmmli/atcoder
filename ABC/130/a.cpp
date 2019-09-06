17977afa3b7561bbb7e15d6df41a317b69b9c040
komoriyuichiro <thekomori1113@gmail.com>
2019-09-06 14:36:52 +0900

    [AC] ABC/130/a.cpp

---
+++ ABC/130/a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+#include <math.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int x, a; cin >> x >> a;
+  int ans = x < a ? 0 : 10;
+
+  cout << ans << endl;
+  return 0;
+}
