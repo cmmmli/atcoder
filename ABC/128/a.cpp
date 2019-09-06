49d975b987c9fc50b64c4cd0c586c1e67f450765
komoriyuichiro <thekomori1113@gmail.com>
2019-09-06 14:31:39 +0900

    [AC] ABC/128/a.cpp

---
+++ ABC/128/a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <ctype.h>
+#include <numeric>
+#include <math.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<ll, ll> Pll;
+const ll MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int dx4[4] = { 0, -1, 1, 0 };
+int dy4[4] = { -1, 0, 0, 1 };
+int dx8[8] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+int dy8[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+
+int main() {
+  int a, p; cin >> a >> p;
+
+  cout << (3 * a + p) / 2 << endl;
+  return 0;
+}
